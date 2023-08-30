6e35e0b28a966b77853a43ff82c12c1d059a111c
Austin Gregg-Smith <blooop@gmail.com>
2023-08-30 12:24:05 +0000

    remove unused classes

--- phase_change_mug/plot_temps.py
+++ phase_change_mug/plot_temps.py
@@ -4,11 +4,7 @@ import hvplot.pandas  # noqa pylint :ignore
 import bencher as bch
 from phase_change_mug.publish_args import publish_args
 
-class MugTemp(bch.ParametrizedSweep):
-    pass
-
-
-bench = bch.Bench("mug_temps",MugTemp())
+bench = bch.Bench("mug_temps")
 
 df = pd.read_csv("phase_change_mug/data.csv")
 ds = hv.Dataset(df)
@@ -20,6 +16,5 @@ bench.append(ds.to(hv.Table))
 bench.append_markdown("""Plot of time vs temperature for 260g of water""", "Time vs Temperature")
 cv = df.hvplot.line(x="Time", y=["temp_mug", "temp_bee", "temp_air"], line_width=2, legend="right")
 bench.append(cv)
-
 bench.publish(publish_args)
 bench.show()
