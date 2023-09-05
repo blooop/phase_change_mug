94904b9cee419a2fe90a1783dd976298d078dce5
Austin Gregg-Smith <blooop@gmail.com>
2023-09-05 18:52:28 +0000

    fix linting errors

--- phase_change_mug/mqtt_client.py
+++ phase_change_mug/mqtt_client.py
@@ -6,26 +6,27 @@ from paho.mqtt import client as mqtt_client
 import time
 import json
 from datetime import datetime
-class TemperatureSensor:
 
+
+class TemperatureSensor:
     def __init__(self) -> None:
         self.topic = "tele/sonoff/SENSOR"
         # self.topic = "#"
         self.client = self.connect_mqtt()
-        self.subscribe(self.client) 
+        self.subscribe(self.client)
 
-        self.temperature=0.  
+        self.temperature = 0.0
         self.start_time = None
-        self.time_relative= None
-        self.time_abs=None  
-
+        self.time_relative = None
+        self.time_abs = None
 
     def connect_mqtt(self) -> mqtt_client:
-        broker = '192.168.0.17'
+        broker = "192.168.0.17"
         port = 1883
         # Generate a Client ID with the subscribe prefix.
-        client_id = f'subscribe-{random.randint(0, 100)}'
-        def on_connect(client, userdata, flags, rc):
+        client_id = f"subscribe-{random.randint(0, 100)}"
+
+        def on_connect(client, userdata, flags, rc):  # noqa pylint :ignore
             if rc == 0:
                 print("Connected to MQTT Broker!")
             else:
@@ -34,44 +35,47 @@ class TemperatureSensor:
         client = mqtt_client.Client(client_id)
         # client.username_pw_set(username, password)
         client.on_connect = on_connect
-        client.connect(broker, port,10)
+        client.connect(broker, port, 10)
         client.loop_start()
         return client
 
-    def on_message(self,client, userdata, msg):
+    def on_message(self, client, userdata, msg):  # noqa pylint :ignore
         decoded = msg.payload.decode()
         print(f"Received `{decoded}` from `{msg.topic}` topic")
         dat = json.loads(decoded)
-        self.time_abs = datetime.strptime(dat["Time"], '%Y-%m-%dT%H:%M:%S')
-        self.temperature = float(dat["DS18B20"]["Temperature"]) 
+        self.time_abs = datetime.strptime(dat["Time"], "%Y-%m-%dT%H:%M:%S")
+        self.temperature = float(dat["DS18B20"]["Temperature"])
 
         if self.start_time is None:
-            self.start_time = self.time_abs        
+            self.start_time = self.time_abs
 
-        self.time_relative = self.time_abs- self.start_time 
+        self.time_relative = self.time_abs - self.start_time
         print(self.time_abs)
-        print(self.time_relative)   
+        print(self.time_relative)
         print(self.temperature)
 
-    def subscribe(self,client: mqtt_client):
+    def subscribe(self, client: mqtt_client):
         client.subscribe(self.topic)
         client.on_message = self.on_message
 
         return client
 
     def get_data(self):
-        
-        while self.start_time is None:                        
+        while self.start_time is None:
             print("waiting for data...")
             time.sleep(1)
 
-        return {"time_abs":self.time_abs,"time_relative":self.time_relative,"temperature":self.temperature}
+        return {
+            "time_abs": self.time_abs,
+            "time_relative": self.time_relative,
+            "temperature": self.temperature,
+        }
 
 
-if __name__ == '__main__':
-    tmon= TemperatureSensor()
+if __name__ == "__main__":
+    tmon = TemperatureSensor()
 
     while 1:
-        dat =tmon.get_data()
-        print(dat)
+        dat1 = tmon.get_data()
+        print(dat1)
         time.sleep(10)
