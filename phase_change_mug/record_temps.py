4026baaa2327314f5c8a01bcba789c410ba11cf0
Austin Gregg-Smith <blooop@gmail.com>
2023-09-05 19:18:23 +0000

    minor cleanup

--- phase_change_mug/record_temps.py
+++ phase_change_mug/record_temps.py
@@ -65,12 +65,9 @@ res = bench.plot_sweep(
     result_vars=[TemperatureRecorder.param.temperature],
     const_vars=TemperatureRecorder.get_input_defaults(),
 )
-# const_vars=TemperatureRecorder.get_input_defaults([TemperatureRecorder.param.mug.with_const(MugWallType.glass)]))
 
-# bench.append("Temperature vs Time per mug")
 bench.append(res.summarise_sweep())
 bench.append(res.to_curve().overlay().opts(width=500, height=500, ylim=(45, 92)))
 bench.append(res.to_hv_dataset().to(hv.Table), "Temperature vs Time per mug")
-# bench.append(res.ds.to_dataframe().hvplot.line(x=["time"],y=["temperature"]))
 # bench.save_index()
 bench.show()
