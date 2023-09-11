f9877d0b76c187479b890407b1ad989960f19843
Austin Gregg-Smith <blooop@gmail.com>
2023-09-11 15:57:07 +0000

    remove unused code

--- phase_change_mug/temperature_recorder.py
+++ phase_change_mug/temperature_recorder.py
@@ -35,28 +35,3 @@ class TemperatureRecorderBase(bch.ParametrizedSweep):
         self.temperature = self.temp_sense.temperature
         return self.get_results_values_as_dict()
 
-
-# run_cfg = bch.BenchRunCfg()
-# run_cfg.use_sample_cache = True
-# run_cfg.only_hash_tag = True
-# run_cfg.repeats = 1
-# # run_cfg.overwrite_sample_cache=True
-# # run_cfg.use_cache = True
-# # run_cfg.over_time=True
-# run_cfg.auto_plot = False
-# bench = bch.Bench("mug_temps", TemperatureRecorder(), run_cfg=run_cfg)
-
-# res = bench.plot_sweep(
-#     "Time vs Temperature",
-#     input_vars=[TemperatureRecorder.param.time, TemperatureRecorder.param.mug],
-#     result_vars=[TemperatureRecorder.param.temperature],
-#     const_vars=TemperatureRecorder.get_input_defaults(),
-# )
-
-
-
-# bench.append(res.summarise_sweep())
-# bench.append(res.to_curve().overlay().opts(width=500, height=500, ylim=(45, 92)))
-# bench.append(res.to_hv_dataset().to(hv.Table), "Temperature vs Time per mug")
-# # bench.save_index()
-# bench.show()
