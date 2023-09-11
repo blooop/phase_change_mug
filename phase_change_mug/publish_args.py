eb98961eb8d04b24c31543a9023330b5e185ddfd
Austin Gregg-Smith <blooop@gmail.com>
2023-09-11 16:26:03 +0000

    remove publish args

--- phase_change_mug/publish_args.py
+++
@@ -1,8 +0,0 @@
-from typing import Tuple
-
-
-def publish_args(branch_name) -> Tuple[str, str]:
-    return (
-        "https://github.com/blooop/phase_change_mug.git",
-        f"https://github.com/blooop/phase_change_mug/blob/{branch_name}",
-    )
