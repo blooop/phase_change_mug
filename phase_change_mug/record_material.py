4bd1b65cc9229c23da0ff4e26ca093e1fa91fad9
Austin Gregg-Smith <blooop@gmail.com>
2023-11-29 20:00:40 +0000

    [wip] last state

--- phase_change_mug/record_material.py
+++ phase_change_mug/record_material.py
@@ -2,17 +2,15 @@ import holoviews as hv
 import bencher as bch
 from enum import auto
 from strenum import StrEnum
-
+import pandas as pd
 from phase_change_mug.temperature_recorder import TemperatureRecorderBase
 
 time_res = 1.0
 duration = 180.0
 
 
-class Substance(StrEnum):
-    bees_wax = auto()
-    soy_wax = auto()
-    # cocunut_wax=auto()
+# soy_wax = auto()
+# cocunut_wax=auto()
 
 
 class TemperatureRecorderSubstance(TemperatureRecorderBase):
@@ -21,6 +19,22 @@ class TemperatureRecorderSubstance(TemperatureRecorderBase):
     )
 
     substance = bch.EnumSweep(Substance, units="")
+
+    def __call__(self, **kwargs):
+        df = pd.read_csv("cocuno.csv")
+        # df.set_index("time",inplace=True)
+        # print(df)
+        tm = kwargs["time"]
+        dat =df.loc[df["time"]==tm]["temperature"]
+        self.temperature = dat.values[0]
+        self.temperature=100.
+
+        # print(self.temperature)
+
+        # return dict(temperature=dat)
+
+
+
 
 
 def material_temps(
@@ -45,11 +59,18 @@ def material_temps(
         const_vars=TemperatureRecorderSubstance.get_input_defaults(),
     )
 
+    # df =res.get_dataframe()
+    # print(df)
+    # df.to_csv("cocuno.csv")
+
+
     bench.report.append_tab(res.summarise_sweep())
-    bench.report.append(res.to_curve().overlay().opts(width=500, height=500))
+    # bench.report.append(res.to_curve().overlay().opts(width=500, height=500))
     bench.report.append(res.to_hv_dataset().to(hv.Table), "Temperature vs Time per mug")
     return bench
 
 
 if __name__ == "__main__":
     material_temps().show()
+
+
